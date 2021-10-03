4794361585523e4deaceb69932b715b1ec8316df
florenciaPH <f.pizarro03@ufromail.cl>
2021-10-03 15:31:40 -0300

    se sube ejercicios clase 6

---
+++ ejercicios-6/ejercicio1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+ int main() {
+
+int numero1=0; 
+int numero2=0;
+int suma =0;
+printf("Hola ingrese un numero\n");
+scanf( "%i", &numero1);
+printf("ingrese otro numero\n");
+scanf( "%i", &numero2 );
+if (numero2 == 1 ) {
+
+suma = numero1 + numero2;
+
+}
+printf("la suma es :%i\n", suma);
+
+
+  return 0;
+ }
+ 
